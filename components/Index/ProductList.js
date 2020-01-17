019c07d8311c9c13e42fede0bcd1bdaaee800cfe
David Tanner <tanner.davide@gmail.com>
2020-01-16 20:50:57 -0700

    page updates

--- components/Index/ProductList.js
+++ components/Index/ProductList.js
@@ -5,13 +5,14 @@ function ProductList({ products }) {
     return products.map(product => ({
       header: product.name,
       image: product.mediaUrl,
+      meta: `$${product.price}`,
       color: 'teal',
       fluid: true,
       childkey: product._id,
       href: `/product?_id=${product._id}`
     }));
   }
-  return <Card.Group items ={mapProductsToItems(products)} />
+  return <Card.Group stackable itemsPerRow="3" centered items ={mapProductsToItems(products)} />
 };
 
 export default ProductList;
--- pages/api/products.js
+++ pages/api/products.js
@@ -5,6 +5,6 @@ import connectDb from '../../utils/connectDb';
 connectDb();
 
 export default async (req, res) => {
-    const products = await Product.find()
+    const products = await Product.find();
     res.status(200).json(products)
 };
